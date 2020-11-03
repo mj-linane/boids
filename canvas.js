4ef71a532bcb7dd85b481b30fb135bdca53b1433
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2020-11-02 19:40:14 -0500

    remove converted javascript

--- canvas.js
+++ canvas.js
@@ -1,138 +1,89 @@
-const flock = [];
-const obstacles = [];
-const obstaclePositionX = [];
-const obstaclePositionY = [];
-const obstaclePosition = [];
-const obstacleDiameter = [];
+const flock = []
+const obstacles = []
+const obstaclePositionX = []
+const obstaclePositionY = []
+const obstaclePosition = []
+const obstacleDiameter = []
 
-let uiBoxWidth = 200;
-let uiReference;
-let sliderWidth = uiBoxWidth/2;
-let circleStroke = 10;
+let uiBoxWidth = 200
+let circleStroke = 10
+let alignmentSlider,
+  cohesionSlider,
+  separationSlider,
+  obstacleAvoidanceSlider,
+  edgeAvoidanceSlider,
+  mouseAvoidanceSlider,
+  maxForceSlider,
+  maxSpeedSlider,
+  numObstacleSlider,
+  perceptionSlider,
+  numBoidSlider
 
-let alignmentSlider, cohesionSlider, separationSlider;
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(51)
 
-function setup()
-{
-    createCanvas(windowWidth, windowHeight);
-    background(51);
-    uiReference = windowHeight/16;
-
-
-    //Sliders:
-    alignmentSlider = createSlider(0, 2, 1, .1);
-    alignmentSlider.position(uiBoxWidth / 8, uiReference);
-    alignmentSlider.style('width', '150px');
-
-    cohesionSlider = createSlider(0, 2, 1, .1);
-    cohesionSlider.position(uiBoxWidth / 8, uiReference + 80);
-    cohesionSlider.style('width', '150px');
-
-    separationSlider = createSlider(0, 2, 1, .1);
-    separationSlider.position(uiBoxWidth / 8, uiReference + 160);
-    separationSlider.style('width', '150px');
-
-    obstacleAvoidanceSlider = createSlider(0, 2, 1, .1);
-    obstacleAvoidanceSlider.position(uiBoxWidth / 8, uiReference + 240);
-    obstacleAvoidanceSlider.style('width', '150px');
-
-    edgeAvoidanceSlider = createSlider(0, 2, 1, .1);
-    edgeAvoidanceSlider.position(uiBoxWidth / 8, uiReference + 320);
-    edgeAvoidanceSlider.style('width', '150px'); 
-
-    mouseAvoidanceSlider = createSlider(0, 2, 1, .1);
-    mouseAvoidanceSlider.position(uiBoxWidth / 8, uiReference + 400);
-    mouseAvoidanceSlider.style('width', '150px'); 
-    
-    maxForceSlider = createSlider(0, 2, 1, .1); 
-    maxForceSlider.position(uiBoxWidth / 8, uiReference + 480);
-    maxForceSlider.style('width', '150px');  
-
-    maxSpeedSlider = createSlider(0, 20, 10, 1); 
-    maxSpeedSlider.position(uiBoxWidth / 8, uiReference + 560);
-    maxSpeedSlider.style('width', '150px'); 
-
-    numObstacleSlider = createSlider(0, 20, 0, 1); 
-    numObstacleSlider.position(uiBoxWidth / 8, uiReference + 640);
-    numObstacleSlider.style('width', '150px'); 
-
-    perceptionSlider = createSlider(0, 200, 100, 10);
-    perceptionSlider.position(uiBoxWidth / 8, uiReference + 720);
-    perceptionSlider.style('width', '150px');
-
-    numBoidSlider = createSlider(0, 200, 100, 10); 
-    numBoidSlider.position(uiBoxWidth / 8, uiReference + 800);
-    numBoidSlider.style('width', '150px'); 
-    
-    
+  //Get Slider Elements
+  alignmentSlider = select('#alignment-slider')
+  cohesionSlider = select('#cohesion-slider')
+  separationSlider = select('#separation-slider')
+  obstacleAvoidanceSlider = select('#obstacle-avoidance-slider')
+  edgeAvoidanceSlider = select('#edge-avoidance-slider')
+  mouseAvoidanceSlider = select('#mouse-avoidance-slider')
+  maxForceSlider = select('#max-force-slider')
+  maxSpeedSlider = select('#max-speed-slider')
+  numObstacleSlider = select('#number-obstacles-slider')
+  perceptionSlider = select('#perception-slider')
+  numBoidSlider = select('#number-boid-slider')
 }
 
-function draw()
-{
-    uiReference = windowHeight/16;
-    background(51);
-    
-    fill('#e65a5a');
-    noStroke();
-    
-    rect(0, 0, uiBoxWidth, windowHeight);
+function draw() {
+  // Update UI Displays With Slider Values
+  select('#alignment-slider-display').html(alignmentSlider.value())
+  select('#cohesion-slider-display').html(cohesionSlider.value())
+  select('#separation-slider-display').html(separationSlider.value())
+  select('#obstacle-avoidance-slider-display').html(
+    obstacleAvoidanceSlider.value(),
+  )
+  select('#edge-avoidance-slider-display').html(edgeAvoidanceSlider.value())
+  select('#mouse-avoidance-slider-display').html(mouseAvoidanceSlider.value())
+  select('#max-force-slider-display').html(maxForceSlider.value())
+  select('#max-speed-slider-display').html(maxSpeedSlider.value())
+  select('#number-obstacles-slider-display').html(numObstacleSlider.value())
+  select('#perception-slider-display').html(perceptionSlider.value())
+  select('#number-boid-slider-display').html(numBoidSlider.value())
 
-    for (let i=flock.length; i<numBoidSlider.value(); i++)
-    {
-        flock.push(new Boid());
-    }
+  background(51)
+  noStroke()
 
-    for (let i=numBoidSlider.value(); i<flock.length; i++)
-    {
-        flock.pop();
-    }
+  for (let i = flock.length; i < numBoidSlider.value(); i++) {
+    flock.push(new Boid())
+  }
 
-    for (let i=obstacles.length; i<numObstacleSlider.value(); i++)
-    {
-        obstacles.push(new Obstacle());
-    }
+  for (let i = numBoidSlider.value(); i < flock.length; i++) {
+    flock.pop()
+  }
 
-    for (let i=numObstacleSlider.value(); i<obstacles.length; i++)
-    {
-        obstacles.pop();
-        obstacleDiameter.pop();
-        obstaclePositionX.pop();
-        obstaclePositionY.pop();
-        obstaclePosition.pop();
-    }
- 
+  for (let i = obstacles.length; i < numObstacleSlider.value(); i++) {
+    obstacles.push(new Obstacle())
+  }
 
+  for (let i = numObstacleSlider.value(); i < obstacles.length; i++) {
+    obstacles.pop()
+    obstacleDiameter.pop()
+    obstaclePositionX.pop()
+    obstaclePositionY.pop()
+    obstaclePosition.pop()
+  }
 
-    //Text Labels
-    textSize(20);
-    textAlign(CENTER);
-    fill('white');
-    textFont('Oswald');
-    text('Alignment: ' + alignmentSlider.value(), uiBoxWidth/2, uiReference-10);
-    text('Cohesion: ' + cohesionSlider.value(), uiBoxWidth/2, uiReference+70);
-    text('Separation: ' + separationSlider.value(), uiBoxWidth/2, uiReference+150);
-    text('Obstacle Avoidance: ' + obstacleAvoidanceSlider.value(), uiBoxWidth/2, uiReference+230);
-    text('Edge Avoidance: ' + edgeAvoidanceSlider.value(), uiBoxWidth/2, uiReference+310);
-    text('Cursor Avoidance: ' + mouseAvoidanceSlider.value(), uiBoxWidth/2, uiReference+390);
-    text('Max Force: ' + maxForceSlider.value(), uiBoxWidth/2, uiReference+470);
-    text('Max Velocity: ' + maxSpeedSlider.value(), uiBoxWidth/2, uiReference+550);
-    text('# Obstacles: ' + numObstacleSlider.value(), uiBoxWidth/2, uiReference+630);
-    text('Perception: ' + perceptionSlider.value(), uiBoxWidth/2, uiReference+710);
-    text('# Boids: ' + numBoidSlider.value(), uiBoxWidth/2, uiReference+790);
-    
+  for (let boid of flock) {
+    boid.edges()
+    boid.flock(flock)
+    boid.update()
+    boid.show()
+  }
 
-    for (let boid of flock)
-    {
-        boid.edges();
-        boid.flock(flock);
-        boid.update();
-        boid.show();
-    }
-
-    for (let obstacle of obstacles)
-    {
-        obstacle.show();
-    }
+  for (let obstacle of obstacles) {
+    obstacle.show()
+  }
 }
-
-
